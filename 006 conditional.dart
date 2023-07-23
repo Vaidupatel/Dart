ef98ac2fb08442570227cf86b7462de605ff1a40
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-07-23 14:21:29 +0530

    Create 006 conditional.dart
    This file contain the code which explain the conditional properties of dart language.

---
+++ 006 conditional.dart
@@ -0,0 +1,31 @@
+main() {
+  var a = 500;
+  var b = 50;
+
+  if (a > 200 || b < 10) {
+    print("Block 1");
+  } else {
+    print("Block 2");
+  }
+
+  // if(a>200 && b>100){
+  // //  Condition 2 is true.
+  // //   if(b>100){     // Nested if and && are work as same
+  // //     //  When both if are true.
+  // //   }
+  // }
+  // else if(a<50){
+  // //  Condition 2 is true.
+  //   print("Block 2");
+  // }else if(a>80) {
+  //   //  Condition 2 is true.
+  //   print("Block 3");
+  // }else if(a==500){
+  //   //  Condition 2 is true.
+  //   print("Block 4");
+  // }
+  // else{
+  //   // When all condition false.
+  //   print("Else");
+  // }
+}
