9d9de8253642df6d4f6733914078d63ead0f231c
Vaidupatel <100674522+Vaidupatel@users.noreply.github.com>
2023-07-23 09:51:57 +0530

    Create 000 intro.dart
    this file contain the introductory code to the dart language

---
+++ 000 intro.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+void main(){
+
+  print("Welcome to dart practice!");
+//   // stdout.write("Enter your name: ");
+//   // var name = stdin.readLineSync();
+//   // print("Your name  is: $name");
+//
+//   var vaidu = Human();  //Class object can be written with or without new key word
+//
+//   // Declaration of the variables
+//
+//   String a; //The question mark after the variable type said that variable can be null
+//   a = "vaidu"; // Initialization
+//   print(a); // None null variable cant be used
+//   a = "Hii!! vaidu";
+//   print(a);
+//
+//   // Inline declaration
+//   // int b = 10000000000000000000; //This much of value cant be store in int data type
+//   BigInt b;
+//   b = BigInt.parse('1000000000000000000000000000000000000'); // We have to parse String in to the integer for larger data
+//   print(b);
+//
+//   double d = 56.245;
+//   num e = 56.245; // num can be store the both value double and int
+//   print(d);
+//
+//   bool f = false;
+//   if (!f){
+//     print("This is False");
+//   }
+//
+// }
+//
+// class Human{  // class
+//   Human(){    // default constructor
+//   }
+
+
+
+
+}
+
